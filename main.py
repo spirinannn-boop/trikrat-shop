8ef9b6c45c710ff9c9ef528db4abced5fc96db45
spirinannn-boop <spirinannn@gmail.com>
2026-09-20 00:30:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -63,10 +63,8 @@ def home():
 
     </body>
     </html>
-    '''
-    if __name__ == '__main__':
-        port = int(os.environ.get("PORT", 5000))
-        app.run(host="0.0.0.0", port=port)
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
     
 
 
